6eac08818af3dbd012be671ba60607907cfd1d9b
Andrey2121-hash <63515750+Andrey2121-hash@users.noreply.github.com>
2020-10-05 00:57:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.slider-pagination',
+        bulletClass: 'slider-bullet',
+        clickable: true
+    }
+});
